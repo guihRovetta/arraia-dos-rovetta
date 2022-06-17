c2eb1a25329f5b015a6ba268b63116999215bf42
guihRovetta <guilherme020213@hotmail.com>
2022-06-17 14:52:32 -0300

    feat: create NextSeoProvider and pass basic info for SEO

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,5 +1,11 @@
 import {ChakraProvider} from './ChakraProvider';
+import {NextSeoProvider} from './NextSeoProvider';
 
 export const Providers = ({children}: {children: React.ReactNode}) => (
-  <ChakraProvider>{children}</ChakraProvider>
+  <>
+    <ChakraProvider>
+      <NextSeoProvider />
+      {children}
+    </ChakraProvider>
+  </>
 );
