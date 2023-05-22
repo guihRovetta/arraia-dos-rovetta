23f2484bdb0b1d4fa1952899f7e6f15719002fea
guihRovetta <guilherme020213@hotmail.com>
2023-05-22 11:03:47 -0300

    fix: update date in description for SEO

--- src/providers/NextSeoProvider.tsx
+++ src/providers/NextSeoProvider.tsx
@@ -3,6 +3,6 @@ import {DefaultSeo} from 'next-seo';
 export const NextSeoProvider = () => (
   <DefaultSeo
     title="Arraiá dos Rovetta"
-    description="Festa junina da família Rovetta realizada em Julho de 2022"
+    description="Festa junina da família Rovetta realizada em 24 de Junho de 2023"
   />
 );
